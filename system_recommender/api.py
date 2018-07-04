758a520c9fbadcd21b858bab27d051bf5434b0d7
José David Moreno Posada <josdavidmo@gmail.com>
2018-07-04 08:23:38 -0500

    add service

--- system_recommender/api.py
+++ system_recommender/api.py
@@ -10,9 +10,10 @@ from models import Artwork, UserRating
 from rest_framework.response import Response
 from serializers import ArtworkSerializer
 from utils import item_similarity_recommender_py
+from django.contrib.auth.models import User
 
 
-class Recommendation(APIView):
+class RecommendationList(APIView):
     """
     used to manage users recommendations
     """
@@ -48,10 +49,17 @@ class Recommendation(APIView):
     def post(self, request):
         qs = UserRating.objects.all().values('user__id', 'artwork__id', 'rating')
         piece_df_1 = qs.to_dataframe()
-        user_id = request.data["user_id"]
+        username = request.data["user_id"]
         results = request.data["survey"]
+        print username, results
         matrix = []
         for result in results:
+            user = User.objects.get(username=username)
+            userRating = UserRating()
+            userRating.user = user
+            userRating.artwork_id = result["piece"]
+            userRating.rating = result["score"]
+            UserRating.save()
             matrix.append([result["piece"], result["score"]])
         results = pandas.DataFrame(
             matrix, columns=["user__id", "artwork__id", "rating"])
@@ -61,13 +69,16 @@ class Recommendation(APIView):
         pieces = []
         for index, row in recommendations.iterrows():
             pieces.append(row['obra'])
+        print pieces
         return HttpResponse(pieces)
+
 
 class ArtworkList(APIView):
 
     """
-    List all artsworks, or create a new snippet.
+    List all artsworks.
     """
+
     def get(self, request, format=None):
         artworks = Artwork.objects.order_by('?')[:30]
         serializer = ArtworkSerializer(artworks, many=True)
--- system_recommender/urls.py
+++ system_recommender/urls.py
@@ -7,10 +7,10 @@ from django.conf.urls import url
 
 from system_recommender import views
 from system_recommender.api import ArtworkList
-from system_recommender.api import Recommendation
+from system_recommender.api import RecommendationList
 
 urlpatterns = [
     url(r'^artworks/$', ArtworkList.as_view()),
-    url(r'^recommendations/$', Recommendation.as_view()),
+    url(r'^recommendations/$', RecommendationList.as_view()),
     url(r'^registry_survey$', views.registry_survey, name='registry-survey')
 ]
